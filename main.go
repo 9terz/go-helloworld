5ce0ce16de7665b418843ecaa84a35fe14f96daf
9terz <9terz666@gmail.com>
2018-07-21 16:04:10 +0700

    All primitive

--- main.go
+++ main.go
@@ -2,16 +2,100 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/9terz/go-helloworld/gotools"
-	"github.com/9terz/go-helloworld/gotools/stringhelper"
 )
 
 func init() {
-	fmt.Println("kuy")
 }
+
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(gotools.Add(10, 2))
-	fmt.Println(stringhelper.Upper("DOG"))
+	// const (
+	// 	myName     = "Pongsathorn"
+	// 	myLastName = "Roonbong"
+	// )
+	// a := "string"
+	// b := 10
+	// c := 30.4
+	// d := true
+	// fmt.Println(a, b, c, d)
+	// if a == "string" {
+	// 	fmt.Printf("%s is stringify\n", a)
+	// }
+	// if aaa := 10; aaa < 100 {
+	// 	fmt.Println("A is less than 100")
+	// }
+	// a := [...]int{1, 2, 3, 4, 5}
+	// b := []int{1, 2, 3, 4, 5, 6, 7}
+	// fmt.Println(a)
+	// fmt.Println(b)
+
+	// for _, bx := range b {
+	// 	fmt.Println(bx)
+	// }
+	// b = append(b, 10)
+	// a := 10
+	// b := 20
+	// switch {
+	// case a > 10:
+	// 	fmt.Println("a is greater than 10")
+	// case b > 10:
+	// 	fmt.Println("b is greater than 10")
+	// default:
+	// 	fmt.Println("I don't know how to Go")
+	// }
+	// defer fmt.Println("middle")
+	// defer fmt.Println("in the")
+	// fmt.Println("Caught")
+	// var a int
+	// defer func() {
+	// 	a = 20
+	// 	}()
+	// fmt.Println(a)
+	// a := make(map[string]int){
+	// 	"one": 1,
+	// 	"two": 2
+	// 	"three": 3
+	// }
+	// me := Person{
+	// 	FirstName: "ter",
+	// 	LastName:  "najaa",
+	// 	Age:       24,
+	// }
+
+	var people []Person
+	people = append(people,
+		Person{
+			FirstName: "ter",
+			LastName:  "najaa",
+			Age:       24,
+		})
+	people = append(people,
+		Person{
+			FirstName: "terter",
+			LastName:  "najakonde",
+			Age:       26,
+		})
+	for _, v := range people {
+		// res, _ := json.Marshal(v)
+		// fmt.Printf("%+v \n", res)
+		v.GrowUp()
+		fmt.Println(v.FullName())
+	}
+
+}
+
+// FullName ...
+func (p *Person) FullName() string {
+	return fmt.Sprintf("%s %s, %d", p.FirstName, p.LastName, p.Age)
+}
+
+// GrowUp ...
+func (p *Person) GrowUp() {
+	p.Age = p.Age + 1
+}
+
+// Person struct
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
 }
